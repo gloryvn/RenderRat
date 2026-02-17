ff68731d9ca1b95437cc7593aa30b9d89b698d10
SHOP <140153886+gloryvn@users.noreply.github.com>
2026-02-17 17:44:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -411,14 +411,6 @@ async def client_ws(websocket: WebSocket, client_id: str):
                     "results": message.get("results")
                 })
 
-            elif msg_type == "lock_status":
-                await broadcast_to_admins({
-                    "type": "lock_status",
-                    "client_id": client_id,
-                    "input_locked": message.get("input_locked"),
-                    "screen_locked": message.get("screen_locked")
-                })
-
 
             # ── Other data (sysinfo, processes, files…) ───────────
             else:
@@ -525,22 +517,6 @@ async def admin_ws(websocket: WebSocket):
                     
 
             elif msg_type in ("wallpaper_upload_start", "wallpaper_chunk", "wallpaper_upload_done"):
-                if client_id in clients:
-                    await clients[client_id].send_text(json.dumps(message))
-
-
-            elif msg_type == "get_lock_status":
-                if client_id in clients:
-                    await clients[client_id].send_text(json.dumps({
-                        "type": "get_lock_status"
-                    }))
-
-
-            elif msg_type in ("lock_input", "unlock_input", "lock_screen", "unlock_screen"):
-                if client_id in clients:
-                    await clients[client_id].send_text(json.dumps(message))
-
-            elif msg_type in ("lock_image_upload_start", "lock_image_chunk", "lock_image_upload_done"):
                 if client_id in clients:
                     await clients[client_id].send_text(json.dumps(message))
 
